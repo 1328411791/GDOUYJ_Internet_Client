6fad5433a7009dc80e41582b5f25facfba8d4359
liahnu <49276659+1328411791@users.noreply.github.com>
2023-04-19 00:57:27 +0800

    设置管理 (#17)
    * 设置基础属性框
    
    * 更新设置参数管理
    
    * 更新设置参数管理

--- app/src/main/java/com/liahnu/auto_login/view/SettingActivity.java
+++ app/src/main/java/com/liahnu/auto_login/view/SettingActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -38,16 +39,37 @@ public class SettingActivity extends AppCompatActivity {
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     private Switch  doubleStack;
-
     private Button checkUpdate;
-
+    private Button saveConfig;
+    private TextView server;
+    private TextView acId;
+    private TextView tryTimes;
+    private TextView tryTimeout;
     private copyElfs ce;
     private Config config;
+
+
+    @SuppressLint("SetTextI18n")
+    private void setTextView(){
+        if(config==null){
+            showResponse("获取配置文件失败");
+            return;
+        }
+        server.setText(config.getServer());
+        acId.setText(config.getAcid().toString());
+        tryTimes.setText(config.getRetry_times().toString());
+        tryTimeout.setText(config.getRetry_delay().toString());
+    }
 
     private void initConfig(){
         doubleStack =findViewById(R.id.double_stack);
         strictBind =findViewById(R.id.strict_bind);
         checkUpdate = findViewById(R.id.check_update);
+        server = findViewById(R.id.server);
+        acId = findViewById(R.id.ac_id);
+        saveConfig = findViewById(R.id.save_config);
+        tryTimeout = findViewById(R.id.login_timeout);
+        tryTimes = findViewById(R.id.try_times);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             ce = new copyElfs(getBaseContext());
         }
@@ -57,6 +79,7 @@ public class SettingActivity extends AppCompatActivity {
         }else{
             Log.e(TAG, "获取文件失败");
         }
+        setTextView();
     }
 
     @Override
@@ -71,6 +94,23 @@ public class SettingActivity extends AppCompatActivity {
             checkUpdate();
         });
 
+        saveConfig.setOnClickListener(view -> {
+            Log.i(TAG, "保存配置");
+            saveConfig();
+        });
+
+
+
+    }
+
+    private void saveConfig() {
+        config.setAcid(Integer.parseInt(acId.getText().toString()));
+        config.setServer(server.getText().toString());
+        config.setRetry_times(Integer.parseInt(tryTimes.getText().toString()));
+        config.setRetry_delay(Integer.parseInt(tryTimeout.getText().toString()));
+        Log.i(TAG, config.toString());
+        ce.updateConfig(config);
+        showResponse("保存成功");
     }
 
     private void checkUpdate(){
