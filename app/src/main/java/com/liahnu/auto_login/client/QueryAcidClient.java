793aa6809f27395cca336c6224bf33d343d595c8
hhll16384 <113534754+hhll16384@users.noreply.github.com>
2023-09-15 23:26:31 +0800

    1.完善acid自动获取（增加另一种不同情况下的获取方式） 2.添加网络超时限制，避免特定情况下出现长时间停顿

--- app/src/main/java/com/liahnu/auto_login/client/QueryAcidClient.java
+++ app/src/main/java/com/liahnu/auto_login/client/QueryAcidClient.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.util.Log;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import cn.hutool.core.util.StrUtil;
 import okhttp3.OkHttpClient;
@@ -15,7 +16,7 @@ public class QueryAcidClient {
         String id = null;
         try {
 
-            OkHttpClient client = new OkHttpClient();
+            OkHttpClient client = new OkHttpClient.Builder().connectTimeout(3, TimeUnit.SECONDS).build();//增加3秒超时设置避免等待太久
             Request request = new Request.Builder().url("http://8.8.8.8").get().build();
             Response response = client.newCall(request).execute();
             String url = response.request().url().toString();
@@ -32,8 +33,14 @@ public class QueryAcidClient {
                 int begin = body.indexOf("/index_") + 7;
                 int end = body.indexOf(".html", begin);
                 if (begin == 6 || end == -1) {
-                    Log.i("QueryAcidClient","Could not find acid in Response body!");
-                    return -1;
+
+                    //return -1; //在这增加新的一种获取方法
+                    begin = body.indexOf("ac_id=") + 6;
+                    end = body.indexOf("&", begin);
+                    if (begin == 5 || end == -1) {
+                        Log.i("QueryAcidClient","Could not find acid in Response body!");
+                        return -1;
+                    }
                 }
                 id = body.substring(begin, end);
             }
